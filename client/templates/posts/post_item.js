29e345dcef73a405bab22ec804acdbae07e82a3a
Alen Hadžić <alen.hadzic91@gmail.com>
2016-05-30 14:07:08 +0200

    Added delete and logging functionality

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -8,3 +8,18 @@ Template.postItem.helpers({
     return a.hostname;
   }
 });
+
+Template.postItem.events({
+  'click .post_delete': function(event){
+    event.preventDefault();
+    post = Posts.findOne({_id:this._id});
+    Meteor.call('postRemove', post, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+      // show this result but route anyway
+      if (result.post_exists_error)
+        Router.go('postsList', {_id: result._id});
+    });
+  }
+});
--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,11 +1,24 @@
 Template.postsList.helpers({
   posts: function(){
-    return Posts.find({}, {sort: {submitted: -1}});
+    displayMine = Template.instance().showMinePosts.get();
+    if(displayMine){
+        return Posts.find({ userId: Meteor.user()._id }, {sort: {submitted: -1}});
+    }
+    else{
+        return Posts.find({}, {sort: {submitted: -1}});
+    }
   }
 });
 
 Template.postsList.events({
   'change [type=checkbox]': function(){
-    console.log('Regeton montenegro');
+    if(Meteor.user()){
+      showMine = Template.instance().showMinePosts.get();
+      Template.instance().showMinePosts.set(!showMine);
+    }
   }
 });
+
+Template.postsList.created = function (){
+  this.showMinePosts = new ReactiveVar(false);
+}
