6102890e99ba401bfbd2157bfe791a00913a45ec
Alen Hadžić <alen.hadzic91@gmail.com>
2016-06-06 15:35:41 +0200

    Development continued, added new post list design. WYSIWYG editor is now fully functional, aded some controls. Modal dialog for deletion confirmation.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -9,17 +9,49 @@ Template.postItem.helpers({
   }
 });
 
+UI.registerHelper('shortIt', function(stringToShorten, maxCharsAmount){
+  if(!stringToShorten) return;
+  if(stringToShorten.length <= maxCharsAmount){
+    return stringToShorten;
+  }
+  return stringToShorten.substring(0, maxCharsAmount);
+});
+
 Template.postItem.events({
   'click .post_delete': function(event){
     event.preventDefault();
     post = Posts.findOne({_id:this._id});
-    Meteor.call('postRemove', post, function(error, result) {
-      // display the error to the user and abort
-      if (error)
-        return alert(error.reason);
-      // show this result but route anyway
-      if (result.post_exists_error)
-        Router.go('postsList', {_id: result._id});
-    });
-  }
-});
+    swal({
+      title: "Are you sure?",
+      text: "You will not be able to recover this imaginary file!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      closeOnConfirm: false,
+      closeOnCancel: true },
+      // Following users decision to delete file.
+      function(isConfirm){
+        if (isConfirm) {
+          // Flow control of delete action.
+          // Calling Meteor method to delete selected post
+          Meteor.call('postRemove', post, function(error, result) {
+            // display the error to the user and abort
+            if (error){
+              return alert(error.reason);
+            }
+            // show this result but route anyway
+            if (result.post_exists_error){
+              Router.go('postsList', {_id: result._id});
+            }
+          });
+          swal("Deleted!", "Your imaginary file has been deleted.", "success");
+          Router.go('postsList');
+        }
+        else {
+          swal("Cancelled", "Your imaginary file is safe", "error");
+        }
+      });
+    }
+  });
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -2,17 +2,18 @@ Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
     var post = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
+      title: $(e.target).find('[name=post_title]').val(),
+      content: $('.summernote').summernote('code')
     };
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
       return alert(error.reason);
       // show this result but route anyway
-      if (result.postExists)
-      alert('This link has already been posted');
-      Router.go('postPage', {_id: result._id});
+      if (result.postExists){
+        alert('This link has already been posted');
+      }
+      Router.go('postsList');
     });
   }
 });
@@ -23,6 +24,6 @@ Template.postSubmit.helpers({
 
 Template.postSubmit.rendered = function(){
   $('.summernote').summernote({
-  height: 200
-});
+    height: 400
+  });
 }
