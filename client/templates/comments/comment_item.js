b98082887d50d001505c5d24edbb1cbcd061e473
Alen Hadžić <alen.hadzic91@gmail.com>
2016-11-08 16:01:44 +0100

    Fixed comment functionality.

--- client/templates/comments/comment_item.js
+++ client/templates/comments/comment_item.js
@@ -1,14 +1,21 @@
 Template.commentItem.events({
-  'click .comment_item_main_view_info_reply_link': function(event){
+  'click .comment_item_main_view_item_info_reply': function(event){
+    event.stopPropagation();
     event.preventDefault();
-    $("#" + this._id).fadeToggle( "fast");
+    Template.instance().commentFieldVisibility.set(!Template.instance().commentFieldVisibility.get());
   }
 });
 
 Template.commentItem.helpers({
-
+  commentFormHidden: function () {
+    return Template.instance().commentFieldVisibility.get() ? true : false;
+  }
 });
+
+Template.commentItem.created = function(){
+  this.commentFieldVisibility = new ReactiveVar(false);
+};
 
 Template.commentItem.rendered = function(){
 
-}
+};
